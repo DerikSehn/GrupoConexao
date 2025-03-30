5ab17c46168285742e8be39958f2ddd0c32e8d21
Derik Sehn <derikbosing@gmail.com>
2025-03-30 19:17:03 -0300

    remove whitespaces

--- my-app/components/sections/FormSection.tsx
+++ my-app/components/sections/FormSection.tsx
@@ -13,7 +13,7 @@ export default function FormSection({ children, id, title }: Readonly<FormSectio
       <Image src="/bg/cta-bg.jpg" alt="Contato" fill className="object-cover z-0 brightness-[.3] saturate-50 " />          
       <span className="absolute inset-0 bg-gradient-to-b from-primary/5 from-30% to-black  z-[1]"></span>
        <Card className="w-full max-w-lg mx-auto relative z-10 ">
-        <CardHeader>
+        <CardHeader>  
           <CardTitle className="text-2xl text-center font-semibold text-gray-800">{title}</CardTitle>
         </CardHeader>
         <CardContent>
